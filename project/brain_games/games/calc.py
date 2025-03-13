b49117f396233f56beab36cd84092d336de2fe85
sroonla <sr0.onlady@mail.ru>
2025-03-13 19:30:32 +0900

    Перенесены файлы из проекта project

---
+++ project/brain_games/games/calc.py
@@ -0,0 +1,18 @@
+import random
+
+RULE = "What is the result of the expression?"
+
+OPERATORS = ['+', '-', '*']
+MIN_NUMBER = 1
+MAX_NUMBER = 20
+
+
+def get_question_and_answer():
+    num1 = random.randint(MIN_NUMBER, MAX_NUMBER)
+    num2 = random.randint(MIN_NUMBER, MAX_NUMBER)
+    operator = random.choice(OPERATORS)
+
+    question = f"{num1} {operator} {num2}"
+    correct_answer = str(eval(question))  # Вычисляем верный ответ
+
+    return question, correct_answer
